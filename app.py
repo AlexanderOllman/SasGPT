adf12244e3046826eb99c1c2ddcb8825e2900f38
AlexanderOllman <healfheonix@hotmail.com>
2025-04-26 21:31:48 -0700

    changed listen port

--- app.py
+++ app.py
@@ -326,4 +326,4 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("app:app", host="0.0.0.0", port=8005, reload=True) 
+    uvicorn.run("app:app", host="0.0.0.0", port="$PORT", reload=True) 
